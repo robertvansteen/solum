59c12454bc17591d706bc8519a292b4a96f8d0d4
Robert van Steen <rovansteen@me.com>
2014-01-18 15:58:42 +0100

    Moved routes to app folder

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$routes = include __DIR__.'/../src/app.php';
+$routes = include __DIR__.'/../app/routes.php';
 $sc = include __DIR__.'/../src/container.php';
 
 $request = Request::createFromGlobals();
